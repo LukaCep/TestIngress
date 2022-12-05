222d5f72f9ce11cc0aef94383fc1d98f1be2e18c
Luka Čep <lcep@mobilisis.hr>
2022-12-05 10:27:30 +0100

    Add project files.

---
+++ TestIngress/IngressClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIngress
+{
+    public class IngressClass
+    {
+        public void Ispis()
+        {
+            Console.WriteLine("Ispis");
+        }
+    }
+
+    public class UDPSocket
+    {
+        public bool SendMessage(string dns, int port, string message, out string responseMessage)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+            try
+            {
+                if (dns == "")
+                {
+                    dns = "ingress.mobilisis.com";
+                }
+
+                udpClient.Connect(dns, port);
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+                udpClient.Send(sendBytes, sendBytes.Length);
+
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                responseMessage = returnData.ToString();
+                udpClient.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                responseMessage = "Error";
+                return false;
+            }
+        }
+    }
+}
---
+++ TestIngress/Program.cs
@@ -0,0 +1,33 @@
+﻿using TestIngress;
+
+
+Console.WriteLine("DNS");
+string dns = Console.ReadLine();
+Console.WriteLine("Port");
+int port = int.Parse(Console.ReadLine());
+Console.WriteLine("Poruka");
+string message = Console.ReadLine();
+
+UDPSocket klasa = new UDPSocket();
+
+string responseMessage;
+bool response = klasa.SendMessage(dns, port, message, out responseMessage);
+
+Console.WriteLine("");
+Console.WriteLine(responseMessage);
+
+if (response)
+{
+    //nesto
+}
+else
+{
+    //nesto drugo
+}
+
+Console.ReadLine();
+
+/*
+ingress.mobilisis.com
+5333
+*/
